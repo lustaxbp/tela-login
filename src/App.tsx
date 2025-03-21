a5a22387160df48ffcc701b6b27a6c2576e96157
Luís Bordin <luisbordinpereira@gmail.com>
2025-03-21 09:55:55 -0300

    fazendo a parte do formulario de login

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import './App.css'
 import Cabecalho from './components/Cabecalho/Cabecalho';
 import Rodape from './components/Rodape/Rodape';
 import Welcome from './components/welcome/welcome';
+import LoginForm from './components/LoginForm/LoginForm';
 function App() {
 
 
   return (
     <>
     <Cabecalho/>
-    <Welcome/>
+    <LoginForm/>
     <Rodape/>
     </>
   )
